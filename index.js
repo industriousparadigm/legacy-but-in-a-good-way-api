a1f75c75113eb382cb3e84bd34dad0281cc18748
industriousparadigm <dsgmcosta@gmail.com>
2020-03-16 14:43:28 +0000

    add port

--- index.js
+++ index.js
@@ -57,6 +57,10 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+server
+  .listen({
+    port: process.env.PORT || 4000
+  })
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
