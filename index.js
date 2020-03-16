a06d8ff8ac33ced691c5c274a4c6bfddb0bbff44
industriousparadigm <dsgmcosta@gmail.com>
2020-03-16 14:33:47 +0000

    static db working with some basic queries

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
 import { ApolloServer, gql } from 'apollo-server'
 
 const typeDefs = gql`
+  enum Status {
+    PLAYED
+    INTERESTED
+    UNKNOWN
+  }
+
+  type Character {
+    id: ID
+    name: String
+  }
+
   type Game {
-    title: String
-    releaseDate: String
-    likes: Int
-    dislikes: Int
+    id: ID!
+    title: String!
+    releaseDate: String!
+    likes: Int!
+    dislikes: Int!
+    comments: [String]!
+    characters: [Character]!
   }
 
   type Query {
     games: [Game]
+    game(id: ID): Game
   }
 `
 const games = [
   {
+    id: '1',
     title: 'Global Gladiators',
     releaseDate: '1992',
     likes: 373,
-    dislikes: 2
+    dislikes: 2,
+    comments: [],
+    characters: []
   },
   {
+    id: '2',
     title: 'Fantastic Dizzy',
     releaseDate: '1993',
     likes: 90,
-    dislikes: 0
+    dislikes: 0,
+    comments: [],
+    characters: []
   }
 ]
 
 const resolvers = {
   Query: {
-    games: () => games
+    games: () => games,
+    game: (obj, arg, context, info) => games.find(game => game.id === arg.id)
   }
 }
 
